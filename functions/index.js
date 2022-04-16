ec0f61ffe150c7fe737288153bf090b323fd8a6f
James Ross <james@jross.me>
2022-04-16 23:32:22 +0100

    test: use onRequest over onRequestGet

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-export async function onRequestGet({request, next, env}){
+export async function onRequest({request, next, env}){
 	const url = new URL(request.url);
 	if(url.searchParams.has('test')){
 		const lookupFile = new URL(request.url);
