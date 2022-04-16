53c7e7b33595ab430f86c9f329276a4544ed4075
James Ross <james@jross.me>
2022-04-16 23:27:27 +0100

    test: 301 redirect for full extension `env.assets.fetch`

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+export async function onRequestGet({request, next, env}){
+	const url = new URL(request.url);
+	if(url.searchParams.has('test')){
+		const lookupFile = new URL(request.url);
+		lookupFile.pathname = './lookup.html';
+		const asset = await env.ASSETS.fetch(lookupFile);
+		return asset;
+	}
+	return next();
+}
