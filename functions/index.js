39dd144628f1b7ca66a03b0b55da6126fa1949d5
James Ross <james@jross.me>
2022-04-16 23:33:57 +0100

    fix: asset path

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ export async function onRequest({request, next, env}){
 	const url = new URL(request.url);
 	if(url.searchParams.has('test')){
 		const lookupFile = new URL(request.url);
-		lookupFile.pathname = './lookup.html';
+		lookupFile.pathname = './test.html';
 		const asset = await env.ASSETS.fetch(lookupFile);
 		return asset;
 	}
