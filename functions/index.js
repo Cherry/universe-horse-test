fc1cbb3685d2dc3c3bcc29d83cca15cf28d7dcaf
James Ross <james@jross.me>
2022-04-17 02:08:23 +0100

    test: use full test.html filename for fetch

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ export async function onRequest({request, next, env}){
 	const url = new URL(request.url);
 	if(url.searchParams.has('test')){
 		const lookupFile = new URL(request.url);
-		lookupFile.pathname = './test';
+		lookupFile.pathname = './test.html';
 		const lookupReq = new Request(lookupFile.toString(), {
 			cf: request.cf
 		});
